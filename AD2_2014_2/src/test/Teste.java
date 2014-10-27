5c030413aea6238c40347d327cd415f4491b57c0
Failde <johnnyfailde@gmail.com>
2014-10-26 23:51:27 -0200

    Ajeitando os Packages e limpando o que nao interessa

--- AD2_2014_2/src/test/Teste.java
+++ AD2_2014_2/src/test/Teste.java
@@ -1,5 +1,8 @@
-package grafos;
+package test;
+
 import java.io.*;
+import grafos.AD02_2014_2;
+
 public class Teste {	
 	public static void main(String[] args) throws IOException{
 		String[] argumentos = {"/home/johnny/Facul/5º Semestre/Prog III/ADs/Teste/ligacoes_possiveis.txt","/home/johnny/Facul/5º Semestre/Prog III/ADs/Teste/Fazer_ligacoes.txt"};
